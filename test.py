001f46d29b474ec6a1d492f62ad4cd21261d7988
Benoit <benoit.goethals@mil.be>
2020-01-31 15:10:22 +0100

    update

--- test.py
+++ test.py
@@ -1,17 +1,56 @@
-import os
-import sys
-import Domain as Domain
-from sqlalchemy import Column, ForeignKey, Integer, String, MetaData,Table
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship, backref
-from sqlalchemy import create_engine
+class Robot:
 
-metadata = MetaData()
-books = Table('book', metadata,
-  Column('id', Integer, primary_key=True),
-  Column('title', String),
-  Column('primary_author', String),
-)
+    __potential_physical: float
+    __potential_psychic:float
+    def __init__(self, name, build_year, lk=0.5, lp=0.5):
+        self.name = name
+        self.build_year = build_year
+        self.__potential_physical = lk
+        self.__potential_psychic = lp
 
-engine = create_engine('sqlite:///bookstore.db')
-metadata.create_all(engine)
+    @property
+    def condition(self):
+        s = self.__potential_physical + self.__potential_psychic
+        if s <= -1:
+            return "I feel miserable!"
+        elif s <= 0:
+            return "I feel bad!"
+        elif s <= 0.5:
+            return "Could be worse!"
+        elif s <= 1:
+            return "Seems to be okay!"
+        else:
+            return "Great!"
+
+
+class Person:
+  __name: str
+  __forName:str
+
+  def __init__(self,name,forname) -> None:
+    self.__name=name
+    self.__forName=forname
+
+  @property
+  def name(self):
+    return self.__name
+
+  def __set_name__(self, owner, name):
+    self.__name=name
+
+  @name.setter
+  def name(self,name):
+    self.__name=name
+
+
+
+if __name__ == "__main__":
+    x = Robot("Marvin", 1979, 0.2, 0.4)
+    y = Robot("Caliban", 1993, -0.4, 0.3)
+    print()
+    print(y.condition)
+    p=Person("fdsfd","ddsfdsf")
+
+    p.name="dsfdsfdsf"
+    print(p.name)
+
